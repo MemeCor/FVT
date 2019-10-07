b5d93f9a66f61f86855e04d07ab62b82148d53c9
Kostz Leonov <roninmifune444@gmail.com>
2019-10-07 11:25:57 +0300

    lab3

--- lab3/scr4.js
+++ lab3/scr4.js
@@ -1,1 +1,19 @@
-alert
+alert("Есть 2 сту... числа 9 и 3. Чтовы хотите сделать")
+let a= prompt("Введите нужное вам действие с помощью танца")
+let b=9;
+let c=3;
+function func1(n1,n2){alert(n1+n2)};
+function func2(n1,n2){alert(n1-n2)};
+function func3(n1,n2){alert(n1*n2)};
+function func4(n1,n2){alert(n1/n2)};
+if (a==="+") {
+    func1(b,c);
+} else if (a==="-"){
+    func2(b,c)
+} else if (a==="*"){
+    func3(b,c)
+} else if (a==="/"){
+    func4(b,c)
+} else {
+  alert("Ничего не понятно но очень интересно")
+}
